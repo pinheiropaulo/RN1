e164b2c022349272f6711bbeb3404391faf7346f
Paulo Pinheiro <pauloprap57@gmail.com>
2021-01-05 01:40:19 -0300

    Aula Finalizada

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { v4 as uuidv4 } from "uuid";
-import { SwipeListView } from "react-native-swipe-list-view";
+import { Modal } from "react-native";
+
 import styled from "styled-components/native";
-
-import ListaItem from "./components/ListaItem";
-import AddItemArea from "./components/AddItemArea";
-
-import lista from "./Lista";
 
 const Container = styled.SafeAreaView`
   flex: 1;
+  justify-content: center;
+  background-color: #eee;
 `;
 
 const Text = styled.Text`
@@ -19,63 +16,49 @@ const Text = styled.Text`
   padding: 20px;
 `;
 
-const ListaSwipe = styled.TouchableHighlight`
+const Box = styled.View`
   width: 100%;
-  height: 50px;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
   justify-content: center;
-  background-color: red;
+  align-items: center;
+`;
+
+const Botao = styled.Button``;
+
+const BoxBody = styled.View`
+  width: 90%;
+  height: 200px;
+  background-color: #fff;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default function App() {
-  const [items, setItems] = useState(lista);
-
-  function addNewItem(text) {
-    let newItems = [...items];
-    newItems.push({
-      id: uuidv4(),
-      nome: text,
-      done: false,
-    });
-    setItems(newItems);
-  }
-
-  function toggleDone(index) {
-    let newItems = [...items];
-    newItems[index].done = !newItems[index].done;
-    setItems(newItems);
-  }
-
-  const ListaItemSwipe = (props) => {
-    return (
-      <ListaSwipe onPress={props.onDelete} underlayColor="#ff3333">
-        <Text>Deletar</Text>
-      </ListaSwipe>
-    );
-  };
-
-  function deleteItem(index) {
-    let newItems = [...items];
-    newItems = newItems.filter((it, i) => i != index);
-    setItems(newItems);
-  }
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <Container>
       <StatusBar style="light" backgroundColor="#000" translucent={false} />
-      <AddItemArea onAdd={addNewItem} />
-      <SwipeListView
-        data={items}
-        leftOpenValue={90}
-        disableLeftSwipe={true}
-        renderHiddenItem={({ index, item }) => (
-          <ListaItemSwipe onDelete={() => deleteItem(index)} />
-        )}
-        renderItem={({ item, index }) => (
-          <ListaItem onPress={() => toggleDone(index)} data={item} />
-        )}
-        //
-        keyExtractor={(item) => item.id}
-      ></SwipeListView>
+
+      <Botao title="Abrir Modal" onPress={() => setModalVisible(true)} />
+
+      <Modal
+        visible={modalVisible}
+        animationType="fade"
+        transparent={true}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <Box>
+          <BoxBody>
+            <Botao
+              title="Fechar Modal"
+              onPress={() => setModalVisible(false)}
+            />
+          </BoxBody>
+        </Box>
+      </Modal>
     </Container>
   );
 }
